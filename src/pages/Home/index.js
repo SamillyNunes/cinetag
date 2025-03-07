6ba20444cd90413744dce4ab6b37c405ae8c6530
Samilly <samillynunes19@gmail.com>
2025-03-07 17:31:47 -0300

    feat: title component created

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import Banner from "components/Banner";
 import Footer from "components/Footer";
 import Header from "components/Header";
+import Title from "components/Title";
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
         <Banner
           bannerUrl="home"
         />
+        <Title>
+          <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+        </Title>
       </main>
       <Footer />
     </>
