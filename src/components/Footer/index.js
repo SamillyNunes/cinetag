61ed930852133b72f16a70db0072ab18b61183bb
Samilly <samillynunes19@gmail.com>
2025-03-07 17:22:33 -0300

    feat: footer component created

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+export default function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <h3>Desenvolvido com Alura.</h3>
+        </footer>
+    );
+}
