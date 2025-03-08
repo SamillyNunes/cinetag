73115936fc1fd957e72f093dc5e7d72f460bb509
Samilly <samillynunes19@gmail.com>
2025-03-07 21:14:58 -0300

    chore: banner and title added to favorites page

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,7 +1,20 @@
+import Banner from 'components/Banner';
 import styles from './Favorites.module.css';
+import Title from 'components/Title';
 
 export default function Favorites(){
     return (
-        <h1>Tela de favoritos!</h1>
+        <>
+            <Banner
+                bannerUrl='favoritos'
+            />
+            <Title>
+                <h1>Meus Favoritos</h1>
+            </Title>
+
+            <section className={styles.container}>
+                {/* aqui vao os cards... */}
+            </section>
+        </>
     );
 }
