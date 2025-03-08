69d0e050cc1932c0984f32679a81e15472bae1e3
Samilly <samillynunes19@gmail.com>
2025-03-07 21:00:40 -0300

    feat: presenting movies dynamically with db.json file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,31 @@
+import styles from "./Home.module.css";
+
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Footer from "components/Footer";
 import Header from "components/Header";
 import Title from "components/Title";
+import movies from "json/db.json";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <Banner
-          bannerUrl="home"
-        />
+        <Banner bannerUrl="home" />
         <Title>
           <h1>Um lugar para guardar seus vídeos e filmes!</h1>
         </Title>
-        <Card id="1" title="Gato amarelo" cover="https://cdn3.gnarususercontent.com.br/2802-react-praticando/img2.png" />
+        <section className={styles.container}>
+          {movies.map((movie) => (
+            <Card
+              key={movie.id}
+              id={movie.id}
+              title={movie.titulo}
+              cover={movie.capa}
+            />
+          ))}
+        </section>
       </main>
       <Footer />
     </>
