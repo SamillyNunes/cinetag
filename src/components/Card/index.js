d98b79ad74d7aba09e48d5f141026fe82ba738bd
Samilly <samillynunes19@gmail.com>
2025-03-10 10:30:43 -0300

    feat: adding favorite movie in the list inside card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,18 @@
+import { useFavoriteContext } from 'hooks/useFavoriteContext';
 import styles from './Card.module.css';
 import liked from './images/favorite.png';
 import unliked from './images/favorite_outline.png';
 
 export default function Card({id, title, cover}){
+    const { favorites, addFavorite } = useFavoriteContext();
+
     return (
         <div className={styles.container}>
             <img src={cover} alt={title} className={styles.cover} />
             <h2>{title}</h2>
             <img
                 src={liked}
+                onClick={()=> addFavorite({id, title, cover})}
                 alt='Ícone de favoritar filme'
                 className={styles.favorite}
             />
